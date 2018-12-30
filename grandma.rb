ac78e49bec1177b102281e6be8246e8505ddec17
Zev Spilman <zevspilman@yahoo.com>
2018-12-30 21:56:29 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -6,6 +6,7 @@ def speak_to_grandma(speech)
     return "NO, NOT SINCE 1938!"
   elsif speech == "I LOVE YOU GRANDMA!"
   return "I LOVE YOU TOO PUMPKIN!"
+end
   
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
